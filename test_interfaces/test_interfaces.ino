ee4e6ea091b9f3e9f7d2979db0844f14677ad4bb
Pascal Mainini <pascal@mainini.ch>
2016-12-02 19:52:52 +0100

    added code for button + led

--- test_interfaces/test_interfaces.ino
+++ test_interfaces/test_interfaces.ino
@@ -9,37 +9,86 @@
  */
 #include <due_can.h>
 
+const int pin_btn = 2;
+const int pin_led = 3;
+
+boolean start = false;
+
 void setup() {
+    pinMode(pin_btn, INPUT_PULLUP);
+    pinMode(pin_led, OUTPUT);
+
     Serial.begin(115200);
     Serial1.begin(115200);
 
     Can0.begin(CAN_BPS_250K);
     Can1.begin(CAN_BPS_250K);
+
+    digitalWrite(pin_led, HIGH);
+    delay(2000);
+    digitalWrite(pin_led, LOW);
+    Serial.write("Initialized!\n\r");
 }
 
 void loop() {
-    CAN_FRAME frame;
-    frame.id = 0x23;
-    frame.length = 8;
-    frame.data.low = 0xEFBEADDE;
-
-    Serial.write("Testing Serial1 port...\n\r");
-    if(Serial1.available() > 0) {
-        Serial1.write("Received: ");
-        Serial1.write(Serial1.read());
-        Serial1.write("\n\r");
-    } else {
-        Serial1.write("Waiting...\n\r");
+    if(digitalRead(pin_btn) == LOW) {
+        start = true;
+        digitalWrite(pin_led, HIGH);
+        delay(500);
+        digitalWrite(pin_led, LOW);
+        delay(200);
+        digitalWrite(pin_led, HIGH);
+        delay(500);
+        digitalWrite(pin_led, LOW);
+        delay(1000);
     }
-    delay(1000);
 
-    Serial.write("Testing Can0 port...\n\r");
-    frame.data.high = 0x00000000;
-    Can0.sendFrame(frame);
-    delay(1000);
+    if(start) {
+        CAN_FRAME frame;
+        frame.id = 0x23;
+        frame.length = 8;
+        frame.data.low = 0xEFBEADDE;
 
-    Serial.write("Testing Can1 port...\n\r");
-    frame.data.high = 0x11111111;
-    Can1.sendFrame(frame);
-    delay(1000);
+        digitalWrite(pin_led, HIGH);
+        delay(200);
+        digitalWrite(pin_led, LOW);
+        Serial.write("Testing Serial1 port...\n\r");
+        if(Serial1.available() > 0) {
+            Serial1.write("Received: ");
+            Serial1.print(Serial1.readString());
+            Serial1.write("\n\r");
+        } else {
+            Serial1.write("Waiting...\n\r");
+        }
+        delay(1000);
+
+        digitalWrite(pin_led, HIGH);
+        delay(200);
+        digitalWrite(pin_led, LOW);
+        Serial.write("Testing Can0 port...\n\r");
+        frame.data.high = 0x00000000;
+        Can0.sendFrame(frame);
+        Can0.sendFrame(frame);
+        Can0.sendFrame(frame);
+        delay(1000);
+
+        digitalWrite(pin_led, HIGH);
+        delay(200);
+        digitalWrite(pin_led, LOW);
+        Serial.write("Testing Can1 port...\n\r");
+        frame.data.high = 0x11111111;
+        Can1.sendFrame(frame);
+        Can1.sendFrame(frame);
+        Can1.sendFrame(frame);
+        delay(1000);
+
+        digitalWrite(pin_led, HIGH);
+        delay(500);
+        digitalWrite(pin_led, LOW);
+        delay(200);
+        digitalWrite(pin_led, HIGH);
+        delay(500);
+        digitalWrite(pin_led, LOW);
+        start = false;
+    }
 }
