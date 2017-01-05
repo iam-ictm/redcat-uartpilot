2f641f49f48e12019be0f01308902985cd0b0c20
Pascal Mainini <pascal@mainini.ch>
2017-01-05 15:42:38 +0100

    autoformatted

--- test_interfaces/test_interfaces.ino
+++ test_interfaces/test_interfaces.ino
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 BFH - Bern University of Applied Sciences, http://bfh.ch
+ * Copyright (C) 2017 BFH - Bern University of Applied Sciences, http://bfh.ch
  * Author: Pascal Mainini <pascal.mainini@bfh.ch>
  *
  * Licensed under MIT license, see included file LICENSE or
@@ -15,80 +15,80 @@ const int pin_led = 3;
 boolean start = false;
 
 void setup() {
-    pinMode(pin_btn, INPUT_PULLUP);
-    pinMode(pin_led, OUTPUT);
+  pinMode(pin_btn, INPUT_PULLUP);
+  pinMode(pin_led, OUTPUT);
 
-    Serial.begin(115200);
-    Serial1.begin(115200);
+  Serial.begin(115200);
+  Serial1.begin(115200);
 
-    Can0.begin(CAN_BPS_250K);
-    Can1.begin(CAN_BPS_250K);
+  Can0.begin(CAN_BPS_250K);
+  Can1.begin(CAN_BPS_250K);
 
-    digitalWrite(pin_led, HIGH);
-    delay(2000);
-    digitalWrite(pin_led, LOW);
-    Serial.write("Initialized!\n\r");
+  digitalWrite(pin_led, HIGH);
+  delay(2000);
+  digitalWrite(pin_led, LOW);
+  Serial.write("Initialized!\n\r");
 }
 
 void loop() {
-    if(digitalRead(pin_btn) == LOW) {
-        start = true;
-        digitalWrite(pin_led, HIGH);
-        delay(500);
-        digitalWrite(pin_led, LOW);
-        delay(200);
-        digitalWrite(pin_led, HIGH);
-        delay(500);
-        digitalWrite(pin_led, LOW);
-        delay(1000);
+  if (digitalRead(pin_btn) == LOW) {
+    start = true;
+    digitalWrite(pin_led, HIGH);
+    delay(500);
+    digitalWrite(pin_led, LOW);
+    delay(200);
+    digitalWrite(pin_led, HIGH);
+    delay(500);
+    digitalWrite(pin_led, LOW);
+    delay(1000);
+  }
+
+  if (start) {
+    CAN_FRAME frame;
+    frame.id = 0x23;
+    frame.length = 8;
+    frame.data.low = 0xEFBEADDE;
+
+    digitalWrite(pin_led, HIGH);
+    delay(200);
+    digitalWrite(pin_led, LOW);
+    Serial.write("Testing Serial1 port...\n\r");
+    if (Serial1.available() > 0) {
+      Serial1.write("Received: ");
+      Serial1.print(Serial1.readString());
+      Serial1.write("\n\r");
+    } else {
+      Serial1.write("Waiting...\n\r");
     }
+    delay(1000);
 
-    if(start) {
-        CAN_FRAME frame;
-        frame.id = 0x23;
-        frame.length = 8;
-        frame.data.low = 0xEFBEADDE;
+    digitalWrite(pin_led, HIGH);
+    delay(200);
+    digitalWrite(pin_led, LOW);
+    Serial.write("Testing Can0 port...\n\r");
+    frame.data.high = 0x00000000;
+    Can0.sendFrame(frame);
+    Can0.sendFrame(frame);
+    Can0.sendFrame(frame);
+    delay(1000);
 
-        digitalWrite(pin_led, HIGH);
-        delay(200);
-        digitalWrite(pin_led, LOW);
-        Serial.write("Testing Serial1 port...\n\r");
-        if(Serial1.available() > 0) {
-            Serial1.write("Received: ");
-            Serial1.print(Serial1.readString());
-            Serial1.write("\n\r");
-        } else {
-            Serial1.write("Waiting...\n\r");
-        }
-        delay(1000);
+    digitalWrite(pin_led, HIGH);
+    delay(200);
+    digitalWrite(pin_led, LOW);
+    Serial.write("Testing Can1 port...\n\r");
+    frame.data.high = 0x11111111;
+    Can1.sendFrame(frame);
+    Can1.sendFrame(frame);
+    Can1.sendFrame(frame);
+    delay(1000);
 
-        digitalWrite(pin_led, HIGH);
-        delay(200);
-        digitalWrite(pin_led, LOW);
-        Serial.write("Testing Can0 port...\n\r");
-        frame.data.high = 0x00000000;
-        Can0.sendFrame(frame);
-        Can0.sendFrame(frame);
-        Can0.sendFrame(frame);
-        delay(1000);
-
-        digitalWrite(pin_led, HIGH);
-        delay(200);
-        digitalWrite(pin_led, LOW);
-        Serial.write("Testing Can1 port...\n\r");
-        frame.data.high = 0x11111111;
-        Can1.sendFrame(frame);
-        Can1.sendFrame(frame);
-        Can1.sendFrame(frame);
-        delay(1000);
-
-        digitalWrite(pin_led, HIGH);
-        delay(500);
-        digitalWrite(pin_led, LOW);
-        delay(200);
-        digitalWrite(pin_led, HIGH);
-        delay(500);
-        digitalWrite(pin_led, LOW);
-        start = false;
-    }
+    digitalWrite(pin_led, HIGH);
+    delay(500);
+    digitalWrite(pin_led, LOW);
+    delay(200);
+    digitalWrite(pin_led, HIGH);
+    delay(500);
+    digitalWrite(pin_led, LOW);
+    start = false;
+  }
 }
