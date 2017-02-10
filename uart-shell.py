6476e161af6f5b01dd23a92afc0c319e53247d88
Pascal Mainini <pascal@mainini.ch>
2017-02-10 23:15:16 +0100

    initially added

---
+++ uart-shell.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# uart-shell.py
+# Minimalistic shell for interacting with UARTPilot
+#
+# Copyright (C) 2017 BFH - Bern University of Applied Sciences, http://bfh.ch
+# Author: Pascal Mainini <pascal.mainini@bfh.ch>
+#
+# Licensed under MIT license, see included file LICENSE or
+# http://opensource.org/licenses/MIT
+#
+
+import argparse, cmd, serial
+
+ENCODING = 'ascii'
+
+class UARTShell(cmd.Cmd):
+    intro = 'Type help or ? to list commands.\n'
+
+    def __init__(self, serialparams):
+        self.prompt = 'C>'
+        self.calculate_checksum = True
+        self.checksum = ''
+        self.raw = False
+
+        self.serial = serial.Serial(*serialparams)
+        print('Serial port opened: ' + self.serial.name + ' ' + str(serialparams))
+
+        super().__init__()
+        
+    def __send(self, data):
+        if self.raw:
+            message = bytes(data, ENCODING)
+            self.serial.write(message)
+        else:
+            checksum = self.__checksum(data)
+            message = bytes('$' + data + '*' + checksum, ENCODING)
+            self.serial.write(message)
+        print('Sent ' + str(message))
+
+    def __checksum(self, data):
+        if self.calculate_checksum:
+            checksum = 0
+            for b in bytes(data, ENCODING):
+                checksum ^= b
+            return format(checksum, '02x')
+        else:
+            return format(self.checksum, '02x')
+
+    def do_checksum(self, arg):
+        'If no argument is given, automatically calculate checksums; otherwise, use given argument as checksum.'
+        if len(arg) == 0:
+            self.calculate_checksum = True
+            self.checksum = ''
+            print('Automatically calculating checksums')
+        else:
+            self.calculate_checksum = False
+            self.checksum = int(arg, 16)
+            print('Checksum set to ' + format(self.checksum, '02x'))
+    
+    def do_raw(self, arg):
+        'Switches RAW mode. If enabled, any input is sent without processing to UARTPilot'
+        if self.raw:
+            self.raw = False
+            self.prompt = 'C>'
+            print('RAW mode disabled')
+        else:
+            self.raw = True
+            self.prompt = 'R>'
+            print('RAW mode enabled')
+
+    def do_exit(self, arg):
+        'Exits the shell.'
+        self.serial.close()
+        return True
+
+    def default(self, arg):
+        if arg == 'EOF':
+            return self.do_exit(self)
+        else:
+            self.__send(arg)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Shell for UARTPilot')
+    parser.add_argument('-b','--baudrate', default=115200, type=int, help='Baudrate to configure the serial interface')
+    parser.add_argument('-d','--device', default='/dev/ttyUSB0', help='Serial interface device')
+    args = parser.parse_args()
+
+    UARTShell((args.device, args.baudrate)).cmdloop()
