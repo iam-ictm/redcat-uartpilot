054c872d925673a51d30d9153f79226241dad93f
Pascal Mainini <pascal@mainini.ch>
2017-02-17 18:28:54 +0100

    initially added

---
+++ count_messages/count_messages.ino
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 BFH - Bern University of Applied Sciences, http://bfh.ch
+ * Author: Pascal Mainini <pascal.mainini@bfh.ch>
+ *
+ * Licensed under MIT license, see included file LICENSE or
+ * http://opensource.org/licenses/MIT
+ *
+ * Test for verifying transmissions, count the number of correctly received messages.
+ */
+
+#include <UARTPilot.h>
+
+UARTPilot::Parser parser;
+
+const int pin_btn = 2;
+
+int count = 0;
+
+void reset() {
+  Serial.print("Count: ");
+  Serial.print(count, DEC);
+  Serial.println();
+  count = 0;
+}
+
+/**
+ * Standard arduino setup(), initialize things
+ */
+void setup() {
+  pinMode(pin_btn, INPUT_PULLUP);
+
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  
+  reset();
+}
+
+/**
+ * Standard arduino main loop()
+ */
+void loop() {
+  if (digitalRead(pin_btn) == LOW) {
+    reset();
+    delay(1000);
+  }
+
+  int msg_len = -1;
+  while (Serial1.available()) {
+    msg_len = parser.parse(Serial1.read());
+  }
+
+  if (msg_len > 0 && parser.isChecksumCorrect()) {
+    count++;
+  }
+}
+
