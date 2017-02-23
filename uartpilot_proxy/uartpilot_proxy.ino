55bf74ade77f0123e6d20814dd386c2b3168e739
Pascal Mainini <pascal@mainini.ch>
2017-02-23 13:13:01 +0100

    * setting can-filters (we do not receive can messages othewise) * adjusted can-frames to required values * removed memset for initialisation (causes unproper initialisation?)

--- uartpilot_proxy/uartpilot_proxy.ino
+++ uartpilot_proxy/uartpilot_proxy.ino
@@ -21,13 +21,12 @@ UARTPilot::Msg_COD_t* p_cod;
 inline void patchFrame(CAN_FRAME *frame) {
   switch (frame->id) {
     case 0x282:
-      frame->data.bytes[1] = p_cod->steerangle != 0 ? 0xC0 : 0;
+      if (p_cod->speed != 0 || p_cod->steerangle != 0) frame->data.bytes[1] = 0xC0;
+      else frame->data.bytes[1] = 0x00;
       break;
     case 0x382:
+      frame->data.bytes[0] = p_cod->speed;
       frame->data.bytes[3] = p_cod->steerangle;
-      break;
-    case 0x384:
-      frame->data.bytes[3] = p_cod->speed;
       break;
   }
 }
@@ -36,12 +35,21 @@ inline void patchFrame(CAN_FRAME *frame) {
  * Standard arduino setup(), initialize things
  */
 void setup() {
+  Serial.begin(115200);
   Serial1.begin(115200);
   Can0.begin(CAN_BPS_250K);
   Can1.begin(CAN_BPS_250K);
 
+  for (int filter = 0; filter < 7; filter++) {
+    Can0.setRXFilter(filter, 0, 0, false);
+    Can1.setRXFilter(filter, 0, 0, false);
+  }
+
   p_cod = (UARTPilot::Msg_COD_t*) malloc(sizeof(p_cod));
-  memset(p_cod, 0, sizeof(p_cod));
+  p_cod->speed = 0;
+  p_cod->steerangle = 0;
+
+  Serial.print("uartpilot_proxy Initialized!");
 }
 
 /**
